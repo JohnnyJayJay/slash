5be8619c0d042c7448244624ac9af2a2e7df263f
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-07-25 16:13:38 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject slash "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library for handling and routing Discord interactions"
+  :url "https://github.com/JohnnyJayJay/slash"
   :license {}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns slash.core})
