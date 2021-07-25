267d8ffb64bb8a2f72c18703dc322e1b9f4868e3
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-07-25 16:14:34 +0200

    Add group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slash "0.1.0-SNAPSHOT"
+(defproject com.github.johnnyjayjay/slash "0.1.0-SNAPSHOT"
   :description "A library for handling and routing Discord interactions"
   :url "https://github.com/JohnnyJayJay/slash"
   :license {}
