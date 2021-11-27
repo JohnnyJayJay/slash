d4f0aad7b1ece800c181b49134e4e6889461a18e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-11-27 13:05:06 +0100

    Add license to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.github.johnnyjayjay/slash "0.3.0-SNAPSHOT"
   :description "A library for handling and routing Discord interactions"
   :url "https://github.com/JohnnyJayJay/slash"
-  :license {}
+  :license {:name "MIT License"
+            :url "https://mit-license.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns slash.core})
